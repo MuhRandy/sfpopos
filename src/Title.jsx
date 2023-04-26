a116c37320dd3ba48c55622402783e7145138b0c
MuhRandy <muhammadrandy0027@gmail.com>
2023-04-26 12:44:31 +0700

    mengubah & menambah style + menambahkan komponen

---
+++ src/Title.jsx
@@ -0,0 +1,13 @@
+import logo from "./assets/react.svg";
+import "./Title.css";
+
+export default function Title() {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+      <div className="Title-Subtitle">
+        San Francisco Privately Owned Public Open Spaces
+      </div>
+    </div>
+  );
+}
